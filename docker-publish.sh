95f300910b0f3beeb9636f47cac8afd8441af3ee
Stephen Blum <stephen@pubnub.com>
2025-05-20 12:12:32 -0700

    updated docker publish command.

--- docker-publish.sh
+++ docker-publish.sh
@@ -4,3 +4,4 @@
 #docker buildx create --name multiarch --driver docker-container --use
 docker buildx use multiarch
 docker buildx build --platform linux/arm64,linux/amd64 -t pubnub/pubnub-mcp-server:latest --push .
+#docker buildx build --platform linux/arm64,linux/amd64 -t stephenlb/pubnub-mcp-server:latest --push .
