48f06a35275bab127079f4bb554bc3f72784942f
Stephen Blum <stephen@pubnub.com>
2025-05-19 17:50:26 -0700

    multi-platform docker.

--- docker-publish.sh
+++ docker-publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker push stephenlb/pubnub-mcp-server:latest
-docker tag pubnub-mcp-server stephenlb/pubnub-mcp-server:latest
-docker buildx build --platform linux/arm64 -t stephenlb/pubnub-mcp-server:latest --push .
+#docker tag pubnub-mcp-server stephenlb/pubnub-mcp-server:latest
+#docker push stephenlb/pubnub-mcp-server:latest
+#docker buildx create --name multiarch --driver docker-container --use
+docker buildx build --platform linux/amd64,linux/arm64 -t stephenlb/pubnub-mcp-server:latest --push .
